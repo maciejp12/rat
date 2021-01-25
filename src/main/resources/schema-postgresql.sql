153dac578b713bd328e73cbcdded043700fa3542
maciejp12 <maciejpredon@gmail.com>
2021-01-25 16:32:05 +0100

    Updated database schema, changed type of price to NUMERIC(16, 2)

--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -2,22 +2,22 @@ DROP TABLE IF EXISTS user_profile CASCADE;
 DROP TABLE IF EXISTS offer CASCADE;
 
 CREATE TABLE user_profile (
-    user_id Bigserial PRIMARY KEY NOT NULL,
-    username varchar(100) UNIQUE NOT NULL,
-    email varchar(100) UNIQUE NOT NULL,
-    password varchar(100) NOT NULL,
-    phone_number varchar(9),
-    register_date timestamp NOT NULL
+    user_id BIGSERIAL PRIMARY KEY NOT NULL,
+    username VARCHAR(100) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(9),
+    register_date TIMESTAMP NOT NULL
 );
 
 
 CREATE TABLE offer (
-    offer_id Bigserial PRIMARY KEY NOT NULL,
-    title varchar(100) NOT NULL,
-    description varchar(300) NOT NULL,
-    price real NOT NULL,
-    creator Bigserial REFERENCES user_profile (user_id),
-    creation_date timestamp NOT NULL
+    offer_id BIGSERIAL PRIMARY KEY NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(300) NOT NULL,
+    price NUMERIC(16, 2) NOT NULL,
+    creator BIGSERIAL REFERENCES user_profile (user_id),
+    creation_date TIMESTAMP NOT NULL
 );
 
 
